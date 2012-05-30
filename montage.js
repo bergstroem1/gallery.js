1ecef84fd38e4252922b424bf7b4c2086d91d24e
Mattias Bergstrom <bergstroem1@gmail.com>
2012-05-30 21:21:21 +0200

    Added columnMontage

--- montage.js
+++ montage.js
@@ -1,9 +1,15 @@
 ﻿
-function Montage(container, margin) {
+function Montage(container) {
 	this.container = container;
-	this.margin = margin;
-	this.width = container.clientWidth;
-	this.height = container.clientHeight;
+	this.prevWidth = container.clientWidth;
+	this.prevHeight = container.clientHeight;
+	
+	this.montageType = "randomTiles";
+	this.caption = false;
+	this.showCaptionOnHover = false;
+	this.numColumns = 4;
+	this.minColumnWidth;
+	this.margin = 2;
 	
 	this.images = [];
 	this.isCreated = false;
@@ -11,17 +17,33 @@ function Montage(container, margin) {
 	//Rebuild the montage
 	this.rebuild = function() {
 		//Check if size has been changed otherwise rebuild isn't necessary
-		if(this.width != this.container.clientWidth || this.height != this.container.clientHeight){
-			this.createMontage();
-			this.width = this.container.clientWidth;
-	        this.height = this.container.clientHeight;
+		if(this.prevWidth != this.container.clientWidth || this.prevHeight != this.container.clientHeight) {
+			
+			this.createMontage({
+				montageType: this.montageType, 
+				caption: this.caption, 
+				showCaptionOnHover: this.showCaptionOnHover,
+				numColumns: this.numColumns,
+				minColumnWidth: this.minColumnWidth,
+				margin: this.margin
+			});
+			this.prevWidth = this.container.clientWidth;
+	        this.prevHeight = this.container.clientHeight;
 		}
 	}
 	
-	this.createMontage = function(caption, showCaptionOnHover){
-		caption = caption || false;
-		showCaptionOnHover = showCaptionOnHover || false;
+	this.createMontage = function(settings) {
+		//Retrieve settings
+		if(typeof settings != 'undefined') {
+			this.montageType = settings.montageType || this.montageType;
+			this.caption = settings.caption || this.caption;
+			this.showCaptionOnHover = settings.showCaptionOnHover || this.showCaptionOnHover;
+			this.numColumns = settings.numColumns || this.numColumns;
+			this.minColumnWidth = settings.minColumnWidth || undefined;
+			this.margin = settings.margin || this.margin;
+		}
 		
+		//If a montage hasn't been created before, do preparation
 		if(this.isCreated == false) {
 			//Collect all a tags
 			for (var i = 0; i < this.container.childNodes.length; i++) {
@@ -37,12 +59,76 @@ function Montage(container, margin) {
 				this.images[i].childNodes[0].style.position = "relative";
 			}
 		}
-
+		
 		//Empty the container
 		while (this.container.firstChild) {
 			this.container.removeChild(this.container.firstChild);
 		}
-
+		
+		//Create the montage depending on type
+		if(this.montageType == "randomTiles")
+			this.createRandomTiles();
+		else if(this.montageType == "columnMontage")
+			this.createColumnMontage();
+		
+		this.isCreated = true;
+	}
+	
+	this.createColumnMontage = function(){
+		//Calculate numColumns if minColumnWidth is set
+		if(this.minColumnWidth != undefined) {
+			var totalWidth = this.container.clientWidth;
+			this.numColumns = Math.floor(totalWidth / this.minColumnWidth);
+		}
+		
+		//Create the columns
+		var cols = [];
+		var colWidth = this.container.clientWidth / this.numColumns;
+		
+		for(var i = 0; i < this.numColumns; i++) {
+			var col = document.createElement('div');
+			col.style.width = colWidth - this.margin * 2 + "px";
+			col.style.margin = this.margin + "px";
+			col.style.display = "inline-block";
+			col.style.verticalAlign = "top";
+			col.className = "montage-column";
+			cols.push(col);
+			container.appendChild(col);
+		}
+		
+		var colNum = 0;
+		if(this.numColumns > 0) {
+			for(var i = 0; i < this.images.length; i++) {
+				colNum = i % this.numColumns;
+				
+				var image = this.images[i].childNodes[0];
+				image.style.width = "100%";
+				image.style.removeProperty('height');
+				
+				var mainContainer = document.createElement('div');
+				mainContainer.className = "montage-image-main-container";
+				var imageContainer = document.createElement('div');
+				imageContainer.className = "montage-image-container";
+				imageContainer.appendChild(this.images[i]);
+				mainContainer.appendChild(imageContainer);
+				
+				if(this.caption){
+					var descriptionContainer = document.createElement('div');
+					descriptionContainer.className = "montage-image-description-container";
+					var description = document.createTextNode(image.alt);
+					descriptionContainer.appendChild(description);
+					descriptionContainer.style.position = "relative";
+					descriptionContainer.style.top = "0px";
+					
+					mainContainer.appendChild(descriptionContainer);
+				}
+				
+				cols[colNum].appendChild(mainContainer);
+			}
+		}
+	}
+	
+	this.createRandomTiles = function(){
 		var num_images = this.images.length;
 
 		//Divide up the space
@@ -52,7 +138,7 @@ function Montage(container, margin) {
 		var imageContainers = [];
 		//Collect all 'image-container'-elements
 		for (var i = 0; i < allContainers.length; i++) {
-			if (allContainers[i].className == 'montagify-image-container') {
+			if (allContainers[i].className == 'montage-image-container') {
 				imageContainers.push(allContainers[i]);
 			}
 		}
@@ -63,10 +149,9 @@ function Montage(container, margin) {
 
 			imageContainers[i].appendChild(this.images[i]);
 			
-			if(caption == true)
-				this.createCaption(imageContainers[i], this.images[i], showCaptionOnHover);
+			if(this.caption == true)
+				this.createCaption(imageContainers[i], this.images[i], this.showCaptionOnHover);
 		}
-		this.isCreated = true;
 	}
 	
 	this.createCaption = function(imageContainer, image, showCaptionOnHover) {
@@ -81,7 +166,7 @@ function Montage(container, margin) {
 		captionBox.appendChild(caption);
 		imageContainer.appendChild(captionBox);
 		
-		if(showCaptionOnHover){
+		if(this.showCaptionOnHover){
 			captionBox.style.bottom = -captionBox.clientHeight + "px";
 			captionBox.style['-webkit-transition'] = "bottom 0.25s ease-in";
 			captionBox.style['-moz-transition'] = "bottom 0.25s ease-in";
@@ -122,7 +207,7 @@ function Montage(container, margin) {
 			container.style.height = parentContainer.clientHeight - margin * 2 + "px";
 			container.style.margin = margin + "px";
 			container.style.overflow = "hidden";
-			container.className = "montagify-image-container";
+			container.className = "montage-image-container";
 			parentContainer.appendChild(container);
 		}
 	}
@@ -137,7 +222,6 @@ function Montage(container, margin) {
 
 		//Do vertical cut
 		if (verticalOrHorizontalCut < (0.5 * aspect)) {
-			console.log();
 			var cutRatio = 0.4 + Math.random() * (0.2);
 
 			var width1 = Math.floor(parentWidth * cutRatio);
